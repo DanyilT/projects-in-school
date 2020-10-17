9009e0bc309791ec0cf50925eda039de49830bce
TymchukHome <drtymchuk@gmail.com>
2020-10-17 21:32:21 +0300

    08.09

---
+++ home/src/lesson1_line2D/Main.java
@@ -0,0 +1,18 @@
+package lesson1_line2D;    //line2D  //№10(Система Git. Ресурси github. Команди)
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Введите 3-значное число");
+        int a = s.nextInt();
+        int a1 = a/100;
+        int a2 = a/10%10;
+        int a3 = a%10;
+        System.out.println("Число едениц в нём = " + a3);
+        System.out.println("Число десятков в нём = " + a2);
+        System.out.println("Сумма его цифр = " + (a1+a2+a3));
+        System.out.println("Произведение его цифр = " + (a1*a2*a3));
+    }
+}
---
+++ home/src/lesson2_IfElse/Main.java
@@ -0,0 +1,17 @@
+package lesson2_IfElse;    //задачи на ветвление  //№5(Операції порівняння. Формат  boolean.  Оператори if та if-else.)
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Введите рост человека");
+        int a = s.nextInt();
+        Boolean b = a > 180;
+        if (b){
+            System.out.println("Высокий");
+        }else {
+            System.out.println("Не очень высокий");
+        }
+    }
+}
