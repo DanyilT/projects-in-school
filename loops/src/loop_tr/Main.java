ced9c51b31d329d403b0c4b6ecf7c8889ddcf08d
Тимчук <@SCH3.LOCAL>
2020-10-19 14:09:30 +0300

    19.10

--- loops/src/loop_tr/Main.java
+++ loops/src/loop_tr/Main.java
@@ -2,21 +2,31 @@ package loop_tr;
 
 public class Main {
     public static void main(String[] args) {
-        //Первый треугольник
+        //Мой вариант
+        for (int i = 0; i < 10; i++) {
+            for (int j = i; j < 10; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("Первый треугольник 10-1");
         for (int i = 0; i < 10; i++) {
             for (int j = i; j < 10; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-        //Второй треугольник
+        System.out.println("Второй треугольник 1-10");
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print("* ");
             }
             System.out.println();
         }
-        //Третий треугольник
+        System.out.println("Треугольник (3)");
         for (int i = 0; i < 10; i++) {
             for (int j = i; j < 10; j++) {
                 System.out.print("  ");
@@ -26,15 +36,49 @@ public class Main {
             }
             System.out.println();
         }
-        //
+        System.out.println("Треугольник (1)");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = i; j < 10; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("Треугольник (2)");
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j < 10; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("Треугольник (4)");
         for (int i = 0; i < 10; i++) {
             for (int j = i; j < 10; j++) {
                 System.out.print("  ");
             }
             for (int j = 0; j < i; j++) {
-                System.out.print(" * ");
+                System.out.print("* ");
             }
             System.out.println();
         }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j < 10; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println("Треугольник (5)");
+        //дорешаьть
     }
 }
