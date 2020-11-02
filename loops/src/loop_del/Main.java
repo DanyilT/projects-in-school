b3114828ea835e63ee1ae94076c019f5ce3f8906
Тимчук <@SCH3.LOCAL>
2020-11-02 15:06:13 +0200

    02.11

---
+++ loops/src/loop_del/Main.java
@@ -0,0 +1,21 @@
+package loop_del;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Введите произвольное число");
+        int a =s.nextInt();
+        int b = 1;
+        int n = 0;
+        while (b <= a){
+            int r = a % b;
+            if (r == 0){
+                n+=1;
+            }
+            b+=1;
+        }
+        System.out.println("Число имеет " + n + " делителей");
+    }
+}
---
+++ loops/src/loop_sum/Main.java
@@ -0,0 +1,18 @@
+package loop_sum;
+
+import java.util.Scanner;
+
+public class Main {
+    static Scanner s = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a =0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Введите " + (i+1) + " число");
+            int x = s.nextInt();
+            a+=x;
+        }
+        System.out.println("сумма 5 чисел равна " + a);
+        double n =a/5;
+        System.out.println("Среднее 5 чисел " + n);
+    }
+}
