d29c8bef90c9adf372adfc096b5fcc819e77ba67
Тимчук <@SCH3.LOCAL>
2020-11-16 15:09:20 +0200

    16.11

---
+++ InCaps/src/calculator/Controller.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Controller {
+    int add(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+    int dif(int a, int b){
+        return a - b;
+    }
+    int mul(int a, int b){
+        return a * b;
+    }
+}
