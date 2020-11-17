8d0a9de3059cb63f8685c0913649a30010201983
Тимчук <@SCH3.LOCAL>
2020-11-17 16:56:28 +0200

    17.11

--- InCaps/src/calculator/Controller.java
+++ InCaps/src/calculator/Controller.java
@@ -11,4 +11,11 @@ public class Controller {
     int mul(int a, int b){
         return a * b;
     }
+    double div(int a, int b){
+        if (b != 0) {
+            return (double) a / b;
+        }else {
+            return 0;
+        }
+    }
 }
--- InCaps/src/calculator/Main.java
+++ InCaps/src/calculator/Main.java
@@ -2,6 +2,29 @@ package calculator;
 
 public class Main {
     public static void main(String[] args) {
+        Model m1 = new Model();
+        m1.x = 50;
+        m1.y = 12;
+        Model m2 = new Model();
+        m2.x = 100;
+        m2.y = 45;
+        Controller c = new Controller();
+        View v = new View();
 
+        int sum = c.add(m1.x, m1.y);
+        System.out.print("Сумма = ");
+        v.displayInteger(sum);
+
+        int dif = c.dif(m1.x, m1.y);
+        System.out.print("Разница = ");
+        v.displayInteger(dif);
+
+        int mul = c.mul(m2.x, m2.y);
+        System.out.print("Произведение = ");
+        v.displayInteger(mul);
+
+        double div = c.div(m2.x, m2.y);
+        System.out.print("Деление = ");
+        v.displayDouble(div);
     }
 }
