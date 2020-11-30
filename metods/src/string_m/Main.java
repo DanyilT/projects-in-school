9f68c4ac15f46a96390b9b0ecaab221531a3fd8e
TymchukHome <drtymchuk@gmail.com>
2020-11-30 21:50:51 +0300

    30.11

--- metods/src/string_m/Main.java
+++ metods/src/string_m/Main.java
@@ -1,6 +1,9 @@
 package string_m;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
         String a = "JAVA FOREVER";
         int index0 = a.indexOf('S');
@@ -12,19 +15,21 @@ public class Main {
         int index3 = a.indexOf("FOR");
         System.out.println(index3);
         System.out.println();
-        String d = "I am person, human";
-        int index01 = d.indexOf('I');
-        System.out.println(index01);
-        int index02 = d.indexOf("am");
-        System.out.println(index02);
-        int index03 = d.indexOf('p');
-        System.out.println(index03);
-        int index04 = d.indexOf('n', 1);
-        System.out.println(index04);
-        int index05 = d.indexOf('a', 2);
-        System.out.println(index05);
-        int index06 = d.indexOf("man");
-        System.out.println(index06);
+//дз(начало)л
+        String d = "I am person, human/Я человек, и всё; больше ничего не придумал";
+        System.out.print("Введите, то что хотите найти = ");
+        String x = s.nextLine();
+        for(int i = 0; i < d.length(); i++){
+            int index = d.indexOf(x, i);
+            if(index == -1){
+                System.out.println("Больше совпадений не найдено");
+                break;
+            }
+        System.out.println(index);
+        i = index;
+        }
+//дз(конец)
+        System.out.println();
 
         int aa = 40, bb = 34;
         double cc = 56.5, dd = 7.2;
