4f0b5e52a8c68684272d178d2ab6a3cdb58ecada
TymchukHome <drtymchuk@gmail.com>
2020-12-07 17:32:28 +0300

    07.12

---
+++ stroimHouse/src/house/Main.java
@@ -0,0 +1,8 @@
+package house;
+
+public class Main {
+    public static void main(String[] args) {
+        House h = new House("Yellow");//1
+        System.out.println(h.square + " " + h.d.hight + " " + h.color);//12
+    }
+}
