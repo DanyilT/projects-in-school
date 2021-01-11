a2da6f024cf431eb8b742eaa30c0c03ba5643b6e
TymchukHome <drtymchuk@gmail.com>
2021-01-11 22:04:23 +0300

    11.01

--- Inheritance/src/flower/Flower.java
+++ Inheritance/src/flower/Flower.java
@@ -1,29 +1,28 @@
 package flower;
-import static net.mindview.util.Print.*;
 public class Flower {
     int petalCount = 0;
     String s = "initial value";
     Flower(int petals) {
         petalCount = petals;
-        print("Constructor w/ int arg only, petalCount= " + petalCount);
+        System.out.println("Constructor w/ int arg only, petalCount= " + petalCount);
     }
     Flower(String ss) {
-        print("Constructor w/ String arg only, s = " + ss);
+        System.out.println("Constructor w/ String arg only, s = " + ss);
         s = ss;
     }
     Flower(String s, int petals) {
         this(petals);
 //! this(s); // Can’t call two! 
         this.s = s; // Another use of "this" 
-        print("String & int args");
+        System.out.println("String & int args");
     }
     Flower() {
         this("hi", 47);
-        print("default constructor (no args)");
+        System.out.println("default constructor (no args)");
     }
     void printPetalCount() {
 //! this(11); // Not inside non-constructor! 
-        print("petalCount = " + petalCount + " s = "+ s);
+        System.out.println("petalCount = " + petalCount + " s = "+ s);
     }
     public static void main(String[] args) {
         Flower x = new Flower();
