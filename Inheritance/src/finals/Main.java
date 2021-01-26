3df7fa7558c7c34ee8c157a43ea20cc9d4a8636c
TymchukHome <drtymchuk@gmail.com>
2021-01-26 15:54:36 +0200

    26.01

--- Inheritance/src/finals/Main.java
+++ Inheritance/src/finals/Main.java
@@ -5,10 +5,13 @@ public class Main {
         int x = 10;
         x++;
         System.out.println(x);
-        Human h1 = new Human(16);
-        Human h2 = new Human(24);
-        final Human dany = new Human(8);
-        //dany.birthday = 16;
+        Human h1 = new Human("Artem/Fox", "Kyiv/Forest", 60, 16);
+        Human h2 = new Human("Danyil", "Kyiv", 60, 16);
+        final Human dany = new Human("Misha", "Varhadskiy", 50, 16);
+        Human [] AllBody = {h1, h2, dany};
+        for (Human temp : AllBody) {
+            System.out.println(temp);
+        }
         int f = fabrica(99);
         f++;
         System.out.println(f);
@@ -22,7 +25,10 @@ class Human{
     String address;
     int weight;
     final int birthday;
-    Human(int birthday){
+
+    public Human(String NAME, String address, int weight, int birthday) {
+        this.address = address;
+        this.weight = weight;
         this.birthday = birthday;
     }
 }
