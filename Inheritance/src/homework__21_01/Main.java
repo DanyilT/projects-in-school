2485bb674d39a9444908acad5360cbfca2520313
TymchukHome <drtymchuk@gmail.com>
2021-01-31 19:03:19 +0300

    30.01

---
+++ Inheritance/src/homework__21_01/Main.java
@@ -0,0 +1,11 @@
+package homework__21_01;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("Boga", "T", 2003);
+        System.out.println(user);
+
+        User_2 user2 = new User_2("Dany", "Tyma", 2004);
+        System.out.println(user2);
+    }
+}
